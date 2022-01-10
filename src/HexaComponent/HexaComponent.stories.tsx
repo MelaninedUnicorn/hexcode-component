f190bb41bbcf719062835f493aed55480576ef18
Mugisha Joyce-Christiane Kakou <mugisha.kakou@gmail.com>
2022-01-10 20:11:10 +0100

    Created stories

---
+++ src/HexaComponent/HexaComponent.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HexaComponent from './HexaComponent';
+
+export default {
+  title: "HexaComponent"
+};
+
+export const Pink = () => <HexaComponent colorCode="#ff8da1" />;
+
+export const Blue = () => <HexaComponent colorCode="#4666d0" />;
